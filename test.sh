b6ad661e8f4b1577eb811785a55761ad770ca9d1
omsri <38715812+omsri@users.noreply.github.com>
2018-06-07 16:24:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+Name1=$(storageAccountName1)
+Name2=$(storageAccountName2)
+Name3=$(esourcegroup1)
+Name3=$(esourcegroup2)
 touch /tmp/extresults.txt
-echo "$1storageAccountName1 $2storageAccountName1 $3Resourcegroup1 $4Resourcegroup2 >> /tmp/extresults.txt" 
+echo "$Name1 $Name2 $Name3 $Name4 >> /tmp/extresults.txt" 
